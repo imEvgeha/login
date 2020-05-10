5641e093d35d91675cf1d94aecdfbe0d42e594a0
Evgeniy <oskarroker@gmail.com>
2020-05-10 22:06:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,131 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import fire from './fire';
 
 function App() {
+  const [loginInp, setLoginInp] = useState('');
+  const [passInp, setPassInp] = useState('');
+  const [logined, setLogined] = useState(null);
+  const ref = fire.database().ref();
+
+  useEffect(() => {
+    ref.once('value').then(function (snapshot) {
+      if (snapshot.val().logined) {
+        setLogined(true);
+      } else {
+        setLogined(false);
+      }
+    });
+  }, []);
+
+  const handleChange = (event) => {
+    if (event.target.name) {
+      setLoginInp(event.target.value);
+      return;
+    }
+    setPassInp(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    ref.once('value').then(
+      function (snapshot) {
+        if (
+          snapshot.val().login === loginInp &&
+          snapshot.val().pass === Number(passInp) &&
+          logined === false
+        ) {
+          ref.update({
+            logined: true,
+          });
+          logIn();
+        }
+      },
+      function (error) {
+        console.log('Error: ' + error.code);
+      }
+    );
+  };
+
+  const logIn = () => {
+    ref.on(
+      'value',
+      function (snapshot) {
+        if (snapshot.val().logined) {
+          setLogined(true);
+        }
+      },
+      function (error) {
+        console.log('Error: ' + error.code);
+      }
+    );
+    setLoginInp('');
+    setPassInp('');
+  };
+
+  const logOut = () => {
+    ref.update({
+      logined: false,
+    });
+    ref.on(
+      'value',
+      function (snapshot) {
+        if (!snapshot.val().logined) {
+          setLogined(false);
+        }
+      },
+      function (error) {
+        console.log('Error: ' + error.code);
+      }
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {logined ? (
+        <h1
+          onDoubleClick={() => {
+            logOut();
+          }}
+          id="text"
         >
-          Learn React
-        </a>
-      </header>
+          Welcome!
+        </h1>
+      ) : (
+        <>
+          {logined === null ? (
+            <img
+              src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif"
+              alt="loading..."
+            ></img>
+          ) : (
+            <div className="loginField">
+              <form onSubmit={handleSubmit}>
+                <b id="login">
+                  Login:
+                  <input
+                    value={loginInp}
+                    onChange={handleChange}
+                    autoComplete="off"
+                    name="login"
+                  ></input>
+                </b>
+                <br></br>
+                <b id="pass">
+                  Password:
+                  <input
+                    value={passInp}
+                    onChange={handleChange}
+                    autoComplete="off"
+                    type="password"
+                  ></input>
+                </b>
+                <br></br>
+                <input type="submit" id="btn" value="Войти"></input>
+              </form>
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 }
---
+++ src/fire.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+var config = {
+  apiKey: 'AIzaSyD_tw3diht3-LXSyHVEzSbcnNza8u1K2a8',
+  authDomain: 'loggggg.firebaseapp.com',
+  databaseURL: 'https://loggggg.firebaseio.com',
+  projectId: 'loggggg',
+  storageBucket: 'loggggg.appspot.com',
+  messagingSenderId: '502684271773',
+  appId: '1:502684271773:web:9a6983eea7e21c52640074',
+};
+var fire = firebase.initializeApp(config);
+export default fire;
